97e76ab945dfd1f008ad9e98249f04bb76825576
Nhat Trinh <nhatmtrinh95@gmail.com>
2017-10-19 17:11:50 -0700

    finished with tcp client-server, few bugs

--- tcp_client.py
+++ tcp_client.py
@@ -1,4 +1,36 @@
 import socket
-import sys
 
+def count_characters(sentence):
+    sentence = str(sentence)
+    words = sentence.split(' ')
+    char_count = 0
+    
+    for word in words:
+        char_count += len(word)
+        
+    return char_count
+
+#create a socket for client
+client_socket = socket.socket()
+#set arbitrary name for server we are requesting from
+server_name = 'localhost'
+#set the server port for the packet to travel through
+server_port = 10000
+
+#connect the socket with the server's address
+client_socket.connect((server_name,server_port))
+
+#create data to send to server
+client_sentence = input('Enter a sentence to send: ')
+client_character_count = count_characters(client_sentence)
+#send the sentence(data) to the server
+client_socket.send(client_sentence.encode())
+#wait for server to acknowledge and receive server's count of characters
+server_character_count = client_socket.recv(1024)
+server_character_count = server_character_count.decode()
+
+print('Sentence: ' + client_sentence)
+print('Client\'s count: ' + str(client_character_count))
+print('Server\'s count: ' + str(server_character_count))
+client_socket.close()
 
--- tcp_server.py
+++ tcp_server.py
@@ -1,5 +1,4 @@
 import socket
-import sys
 
 def count_characters(sentence):
     sentence = str(sentence)
@@ -11,12 +10,21 @@ def count_characters(sentence):
         
     return char_count
 
-client_socket = socket.socket()
-server_name = 'tcp_server'
-server_port = 10000
 
-client_socket.connect((server_name,server_port))
-sentence = input('Enter a sentence to send: ')
-client_socket.send(sentence.encode())
+#create a socket for the server
+server_socket = socket.socket()
+#set the address of that socket to be localhost port 10000
+server_address = ('',10000)
+#the socket will be binded with the address
+server_socket.bind(server_address)
+#once the socket listens it turns into a server socket
+server_socket.listen(1)
 
-client_socket.close()
+while True:
+    client_socket, client_address = server_socket.accept()
+    client_message = client_socket.recvfrom(2048)
+    print('Server got message: ' + client_message[0].decode())
+    print('Server count: ' + count_characters(client_message[0].decode()))
+    client_socket.send(str(count_characters(client_message[0])).encode())
+
+server_socket.close()
